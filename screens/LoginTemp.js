21dfdc43d8ea5af622aaf4adf24609921d59e86a
@nati4888 <natnaelgizachew4888@gmail.com>
2023-01-19 00:16:59 -0800

    loginpage

--- screens/LoginTemp.js
+++ screens/LoginTemp.js
@@ -1,47 +1,145 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet, TextInput} from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet, TextInput } from 'react-native';
 
 
 const LoginTemp = () => {
-    const [UserName, setUserName] = useState();
-    const [UserId, setUserId] = useState();
-    const [DriverName, setDriverName] = useState();
-    const [DriverId, setDriverId] = useState();
+    const [Name, setName] = useState();
+    const [Id, setId] = useState();
+    const [NamePlaceHolder, setNamePlaceHolder] = useState();
+    const [IdPlaceHolder, setIdPlaceHolder] = useState();
     const [isUser, setisUser] = useState(true)
 
-    const DriverSignup = () => {
-        return (
-            <View style={{ margin:5, borderWidth:1, borderStyle:'solid' }}>
-                <TextInput onChangeText={(Driver)=>{setDriverName(Driver)}} placeholder='Enter your Driver Name'></TextInput>
-                <TextInput onChangeText={(Driver)=>{setDriverId(Driver)}} placeholder='Enter your id'></TextInput>
-                <TouchableOpacity onPress={()=>{}} style={{}}><Text>Submite</Text></TouchableOpacity>
-            </View>
-        )
+    useEffect(() => {
+        SwitchFunction();
+    }, []);
+
+    const SwitchFunction = () => {
+        if (isUser) {
+            setIdPlaceHolder('User Id');
+            setNamePlaceHolder('User Name');
+        }
+        else {
+            setIdPlaceHolder("Drver Id");
+            setNamePlaceHolder('Driver Name');
+        }
     }
 
-
-    const UserSignup = () => {
-        return (
-            <View style={{ margin:5, borderWidth:1, borderStyle:'solid' }}>
-                <TextInput onChangeText={(User)=>{setUserName(User)}} placeholder='Enter your User Name'></TextInput>
-                <TextInput onChangeText={(User)=>{setUserId(User)}} placeholder='Enter your id'></TextInput>
-                <TouchableOpacity onPress={()=>{}} style={{}}><Text>Submite</Text></TouchableOpacity>
-            </View>
-        )
-    }
 
     return (
-        <View style={{flex:1,padding:15}}>
-            <View style={{ flexDirection: 'row',justifyContent:'flex-end' }}>
-                <TouchableOpacity onPress={()=>{setisUser(true)}} style={{ backgroundColor: "black" }}><Text>User</Text></TouchableOpacity>
-                <TouchableOpacity onPress={()=>{setisUser(false)}} style={{ backgroundColor: "red" }}><Text>Driver</Text></TouchableOpacity>
+        <View style={styles.container}>
+            <View>
+                <View style={styles.swicth}>
+                    <TouchableOpacity onPress={() => { setisUser(true) }} style={{ backgroundColor: "black" }}><Text>User</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={() => { setisUser(false) }} style={{ backgroundColor: "red" }}><Text>Driver</Text></TouchableOpacity>
+                </View>
+                <Text style={styles.Headertext}>HeaderTitle</Text>
             </View>
-            <Text>HeaderTitle</Text>
-            {isUser?<DriverSignup/>:<UserSignup/>}
+            <View style={styles.Signin}>
+                <TextInput onChangeText={(Text) => { setName(Text) }} placeholder={NamePlaceHolder}></TextInput>
+                <TextInput onChangeText={(Text) => { setId(Text) }} placeholder={IdPlaceHolder}></TextInput>
+                <TouchableOpacity onPress={() => { }} style={{}}><Text>Submite</Text></TouchableOpacity>
+            </View>
+            <View style={styles.Signup}>
+                <View style={styles.textPrivate}>
+                    <Text style={styles.color_textPrivate}>By registering, you confirm that you accept our </Text>
+                    <TouchableOpacity onPress={() => alert('Terms Clicked!')}>
+                        <Text style={[styles.color_textPrivate, { color: '#e88832' }]}>Terms of service</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.color_textPrivate}> and </Text>
+                    <Text style={[styles.color_textPrivate, { color: '#e88832' }]}>Privacy Policy</Text>
+                </View>
+                {isUser ?
+                    <TouchableOpacity
+                        style={styles.navButton}
+                        onPress={() => { }}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={styles.color_textPrivate}>
+                                Don't have an account?
+                            </Text>
+                            <Text style={styles.navButtonText}>
+                                Sign Up
+                            </Text>
+                        </View>
+                    </TouchableOpacity> : null
+                }
+            </View>
+
         </View>
     )
 }
 
 export default LoginTemp
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f9fafd',
+        flex: 1,
+        padding: 20,
+        justifyContent:'space-between'
+    },
+    swicth: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    switchBtn: {
+
+    },
+    Headertext: {
+        fontSize: 28,
+        marginBottom: 10,
+        color: '#051d5f',
+        alignSelf: 'center'
+    },
+    Signin: {
+        margin: 5,
+        borderWidth: 1,
+        borderStyle: 'solid',
+    },
+    Signup: {
+        marginHorizontal: 5,
+    },
+    navButton: {
+        alignSelf: 'center'
+    },
+    navButtonText: {
+        fontSize: 21,
+        fontWeight: '400',
+        color: '#2e64e5',
+        marginLeft: 5,
+    },
+    textPrivate: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginVertical: 15,
+        justifyContent: 'center',
+    },
+    color_textPrivate: {
+        fontSize: 13,
+        fontWeight: '400',
+        color: 'grey',
+    },
+});
+/**
+    const DriverSignup = () => {
+        return (
+            <View style={{ margin: 5, borderWidth: 1, borderStyle: 'solid' }}>
+                <TextInput onChangeText={(Driver) => { setDriverName(Driver) }} placeholder='Enter your Driver Name'></TextInput>
+                <TextInput onChangeText={(Driver) => { setDriverId(Driver) }} placeholder='Enter your id'></TextInput>
+                <TouchableOpacity onPress={() => { }} style={{}}><Text>Submite</Text></TouchableOpacity>
+            </View>
+        )
+    }
+    //{isUser ? <DriverSignup /> : <UserSignup />}
+
+    const UserSignup = () => {
+        return (
+            <View style={{ margin: 5, borderWidth: 1, borderStyle: 'solid' }}>
+                <TextInput onChangeText={(User) => { setUserName(User) }} placeholder='Enter your User Name'></TextInput>
+                <TextInput onChangeText={(User) => { setUserId(User) }} placeholder='Enter your id'></TextInput>
+                <TouchableOpacity onPress={() => { }} style={{}}><Text>Submite</Text></TouchableOpacity>
+            </View>
+        )
+    }
+ 
+
+ */
--- screens/driver/DriverScreen.js
+++ screens/driver/DriverScreen.js
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+//import React, { useEffect ,useState,useRef} from 'react'
+//import * as tt from '@tomtom-international/web-sdk-maps'
 
 const DriverScreen = () => {
+
+
+
   return (
-    <View>
-      <Text>DriverScreen</Text>
+    <View style={{flex:1}}>
     </View>
   )
 }
@@ -12,3 +15,14 @@ const DriverScreen = () => {
 export default DriverScreen
 
 const styles = StyleSheet.create({})
+/**
+ const mapElement=useRef()
+const [map,setMap]=useState({})
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+    })
+    setMap(map)
+  }, [])
+ */
